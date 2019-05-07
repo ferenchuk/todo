3c0e8f41c61029fdf252256e595d67026127e2cd
Ivan Ferenchuk <if@conciliolabs.com>
2019-05-07 03:33:02 +0300

    feat: save data to localStorage

--- src/index.js
+++ src/index.js
@@ -7,10 +7,16 @@ import './index.css';
 import App from './containers/App';
 import './styles/main.scss';
 
+const persistedState = localStorage.getItem('todoApp') ? JSON.parse(localStorage.getItem('todoApp')) : {}
 const store = createStore(
     reducer,
+    persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(()=>{
+    localStorage.setItem('todoApp', JSON.stringify(store.getState()))
+});
 
 render(
     <Provider store={store}>
